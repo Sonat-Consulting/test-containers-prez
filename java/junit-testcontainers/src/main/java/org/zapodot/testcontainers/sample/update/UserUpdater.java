40b437c82d682e73d1be3c4a68c28136c24947a1
Sondre Eikanger Kvalø <zapodot@gmail.com>
2024-10-24 20:54:39 +0200

    Liten fiks

--- java/junit-testcontainers/src/main/java/org/zapodot/testcontainers/sample/update/UserUpdater.java
+++ java/junit-testcontainers/src/main/java/org/zapodot/testcontainers/sample/update/UserUpdater.java
@@ -2,11 +2,11 @@ package org.zapodot.testcontainers.sample.update;
 
 import org.zapodot.testcontainers.sample.csv.UserWithRoles;
 import org.zapodot.testcontainers.sample.model.Role;
-import org.zapodot.testcontainers.sample.model.User;
 import org.zapodot.testcontainers.sample.repositories.read.RoleReadRepository;
 import org.zapodot.testcontainers.sample.repositories.write.UserWriteRepository;
 
 import java.util.List;
+import java.util.Objects;
 
 public class UserUpdater {
     private final RoleReadRepository roleReadRepository;
@@ -18,10 +18,11 @@ public class UserUpdater {
     }
 
     public long insertUsers(final List<UserWithRoles> usersWithRoles) {
-        return usersWithRoles
-                .stream()
-                .map(userWithRoles -> userWriteRepository.save(userWithRoles.name(), userWithRoles.roles().stream().map(
-                        roleReadRepository::findByName).map(Role::id).toList()))
-                .count();
+        usersWithRoles
+                .forEach(userWithRoles -> userWriteRepository.save(userWithRoles.name(), userWithRoles.roles().stream().map(
+                                roleReadRepository::findByName)
+                        .filter(Objects::nonNull)
+                        .map(Role::id).toList()));
+        return usersWithRoles.size();
     }
 }
